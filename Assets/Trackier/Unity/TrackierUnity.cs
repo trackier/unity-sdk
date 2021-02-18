d8fe595ddc8e738600d82cd18ebb638058fbd0e0
prak24oct <prakharatdit@gmail.com>
2021-02-18 18:58:13 +0530

    Code indentation fixed

--- Assets/Trackier/Unity/TrackierUnity.cs
+++ Assets/Trackier/Unity/TrackierUnity.cs
@@ -4,51 +4,45 @@ using UnityEngine;
 
 namespace com.trackier.sdk
 {
-    public class TrackierUnity : MonoBehaviour
-    {
-        public string AppToken = "";
-        void Awake()
-        {
-            if (IsEditor())
-            {
-                return;
-            }
-
-            TrackierUnity.start(this.AppToken);
-        }
-
-        public static void start(String appToken)
-        {
-            if (IsEditor())
-            {
-                return;
-            }
-
+	public class TrackierUnity : MonoBehaviour
+	{
+		public string AppToken = "";
+		void Awake()
+		{
+			if (IsEditor())
+			{
+				return;
+			}
+		TrackierUnity.start(this.AppToken);
+		}
+		public static void start(string appToken)
+		{
+			if (IsEditor())
+			{
+				return;
+			}
 #if UNITY_ANDROID
-			TrackierAndroid.Start(appToken, "");
+		TrackierAndroid.Start(appToken, "");
 #endif
-        }
-
-        public static void TrackEvent(TrackierEvent te)
-        {
-            if (IsEditor())
-            {
-                return;
-            }
-
+		}
+		public static void TrackEvent(TrackierEvent te)
+		{
+			if (IsEditor())
+			{
+				return;
+			}
 #if UNITY_ANDROID
-			TrackierAndroid.TrackEvent(te);
+		TrackierAndroid.TrackEvent(te);
 #endif
-        }
-
-        private static bool IsEditor()
-        {
+		}
+		private static bool IsEditor()
+		{
 #if UNITY_EDITOR
-			return true;
+		return true;
 #else
-            return false;
+		return false;
 #endif
-        }
-    }
+		}
+	}
 }
 
--- Assets/Trackier/Util/AndroidUtil.cs
+++ Assets/Trackier/Util/AndroidUtil.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class AndroidUtil
 {
 
-    public static AndroidJavaObject CreateJavaMapFromDictainary(IDictionary<string, object> parameters)
-    {
-        AndroidJavaObject javaMap = new AndroidJavaObject("java.util.HashMap");
-        IntPtr putMethod = AndroidJNIHelper.GetMethodID(
-            javaMap.GetRawClass(), "put",
-                "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+	public static AndroidJavaObject CreateJavaMapFromDictainary(IDictionary<string, object> parameters)
+	{
+		AndroidJavaObject javaMap = new AndroidJavaObject("java.util.HashMap");
+		IntPtr putMethod = AndroidJNIHelper.GetMethodID(
+			javaMap.GetRawClass(), "put",
+				"(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
 
-        object[] args = new object[2];
-        foreach (KeyValuePair<string, object> kvp in parameters)
-        {
-            using (AndroidJavaObject k = new AndroidJavaObject(
-                "java.lang.String", kvp.Key))
-            {
-                using (AndroidJavaObject v = new AndroidJavaObject(
-                    "java.lang.String", kvp.Value))
-                {
-                    args[0] = k;
-                    args[1] = v;
-                    AndroidJNI.CallObjectMethod(javaMap.GetRawObject(),
-                            putMethod, AndroidJNIHelper.CreateJNIArgArray(args));
-                }
-            }
-        }
+		object[] args = new object[2];
+		foreach (KeyValuePair<string, object> kvp in parameters)
+		{
+			using (AndroidJavaObject k = new AndroidJavaObject(
+				"java.lang.String", kvp.Key))
+			{
+				using (AndroidJavaObject v = new AndroidJavaObject(
+					"java.lang.String", kvp.Value))
+				{
+					args[0] = k;
+					args[1] = v;
+					AndroidJNI.CallObjectMethod(javaMap.GetRawObject(),
+							putMethod, AndroidJNIHelper.CreateJNIArgArray(args));
+				}
+			}
+		}
 
-        return javaMap;
-    }
+		return javaMap;
+	}
 }
