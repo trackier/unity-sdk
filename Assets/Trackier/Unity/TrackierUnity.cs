c36c7423083c8354251cbbfef4655d4144fb061e
prak24oct <prakharatdit@gmail.com>
2021-02-19 12:13:55 +0530

    Code Indentation Corrected

--- Assets/Trackier/Unity/TrackierUnity.cs
+++ Assets/Trackier/Unity/TrackierUnity.cs
@@ -7,41 +7,56 @@ namespace com.trackier.sdk
 	public class TrackierUnity : MonoBehaviour
 	{
 		public string AppToken = "";
+
 		void Awake()
 		{
 			if (IsEditor())
 			{
 				return;
 			}
-		TrackierUnity.start(this.AppToken);
+
+		    TrackierUnity.start(this.AppToken);
 		}
+
 		public static void start(string appToken)
 		{
 			if (IsEditor())
 			{
 				return;
 			}
-#if UNITY_ANDROID
-		TrackierAndroid.Start(appToken, "");
-#endif
+
+           #if UNITY_ANDROID
+
+		       TrackierAndroid.Start(appToken, "");
+
+           #endif
 		}
+
 		public static void TrackEvent(TrackierEvent te)
 		{
 			if (IsEditor())
 			{
 				return;
 			}
-#if UNITY_ANDROID
-		TrackierAndroid.TrackEvent(te);
-#endif
+
+            #if UNITY_ANDROID
+
+		       TrackierAndroid.TrackEvent(te);
+
+            #endif
 		}
+		  
 		private static bool IsEditor()
 		{
-#if UNITY_EDITOR
-		return true;
-#else
-		return false;
-#endif
+           #if UNITY_EDITOR
+
+		      return true;
+
+           #else
+
+		      return false;
+			  
+           #endif
 		}
 	}
 }
