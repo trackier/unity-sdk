2b1a25edf637bcd23aaa28f7f0994638c0cd69a0
Anuj Agrawal <anujagrawal06@gmail.com>
2022-09-08 16:41:02 +0530

    changed sample to expampleApp

---
+++ exampleApp/Assets/Script/mainScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.trackier.sdk;
+
+public class mainScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     TrackierUnity.start("xxxxxx-d94a-xxxx-a2ae-xxxxxx");
+        
+            /* Event Track */
+            TrackierEvent trackierEvent = new TrackierEvent("Q4YsqBKnzZ");
+            trackierEvent.param1 = "param";
+            trackierEvent.currency = "INR";
+            TrackierUnity.TrackEvent(trackierEvent);
+
+            Debug.Log("Hi, This is the Unity App");     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
