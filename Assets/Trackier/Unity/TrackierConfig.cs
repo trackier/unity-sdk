118a062e2c6c26a11eaacd435baec1982ceffd05
Sanu gupta <sanugupta266@gmail.com>
2022-10-07 23:34:06 +0530

    minor-changes

--- Assets/Trackier/Unity/TrackierConfig.cs
+++ Assets/Trackier/Unity/TrackierConfig.cs
@@ -24,6 +24,7 @@ namespace com.trackier.sdk
         public Action<string> getDeferredDeeplinkDelegate()
         {
             UnityEngine.Debug.Log("getDeferredDeeplinkDelegate()");
+            return this.deferredDeeplinkDelegate;
         }
     }
 }
